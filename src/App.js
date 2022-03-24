d6189989b7df19da82ff81a342d0b24ed093b78a
Timmy Lau <tlau9898@gmail.com>
2022-03-24 01:36:21 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+//my pages
+import HomePage from './pages/HomePage';
+
+
+// install stuff
+import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/umd/popper.min.js';
+
+
+//Routing stuff
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+
+//Error handling 
+import NotFoundPage from './pages/NotFoundPage';
+import PrivacyPage from './pages/PrivacyPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={< HomePage />} exact />
+          <Route path="/Home/Privacy" element={< PrivacyPage />} exact />
+          <Route path="*" element={< NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
